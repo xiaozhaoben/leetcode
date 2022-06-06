406e296f055217aa4902098c7337ddbd3353e7c7
xiaozhao <2502712438@qq.com>
2022-06-06 18:28:48 +0800

    剑指offer49

--- jianzhi/src/jianzhi49.java
+++ jianzhi/src/jianzhi49.java
@@ -38,4 +38,21 @@ public class jianzhi49 {
             sum += Integer.parseInt(str);
         }
     }
+
+    //Leetcode
+//    public int sumNumbers(TreeNode root) {
+//        return dfs(root, 0);
+//    }
+//
+//    public int dfs(TreeNode root, int prevSum) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        int sum = prevSum * 10 + root.val;
+//        if (root.left == null && root.right == null) {
+//            return sum;
+//        } else {
+//            return dfs(root.left, sum) + dfs(root.right, sum);
+//        }
+//    }
 }
