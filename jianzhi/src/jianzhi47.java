e26dbc2de5b47582a764bb47404a0f54d14b95df
xiaozhao <2502712438@qq.com>
2022-06-06 16:38:32 +0800

    剑指offer47

---
+++ jianzhi/src/jianzhi47.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class jianzhi47 {
+
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+
+      }
+  }
+
+
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.left == null && root.right == null && root.val == 0){
+            return null;
+        }
+        return root;
+    }
+}
