6275fbf85c2a0015f17490d288040e478b03eb7f
2502712438@qq.com <13697783370zhao>
2022-06-06 14:27:52 +0800

    剑指offer46

---
+++ jianzhi/src/jianzhi46.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class jianzhi46 {
+
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                root = queue.poll();
+                if (i == size - 1){
+                    ans.add(root.val);
+                }
+                if (root.left != null){
+                    queue.offer(root.left);
+                }
+                if (root.right != null){
+                    queue.offer(root.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
