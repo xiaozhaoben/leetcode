5cf58270ad5ec73b1cb25912e66f0622092ffb1a
xiaozhao <2502712438@qq.com>
2022-06-21 16:29:50 +0800

    剑指97

---
+++ jianzhi/src/jianzhi97.java
@@ -0,0 +1,22 @@
+/*
+* 剑指 Offer II 097. 子序列的数目
+* */
+public class jianzhi97 {
+
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int dp[][] = new int[m + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= m; i++){
+            dp[i][0] = 1;
+            for (int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
