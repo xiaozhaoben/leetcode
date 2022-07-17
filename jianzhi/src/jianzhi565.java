b1c1fc550f24850f0b50e9ac506504bbf792350b
xiaozhao <2502712438@qq.com>
2022-07-17 15:18:49 +0800

    每日7.17

--- jianzhi/src/jianzhi565.java
+++ jianzhi/src/jianzhi565.java
@@ -38,4 +38,22 @@ public class jianzhi565 {
         return max;
     }
 
+    //官方解答
+    class Solution {
+        public int arrayNesting(int[] nums) {
+            int ans = 0, n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                int cnt = 0;
+                while (nums[i] < n) {
+                    int num = nums[i];
+                    nums[i] = n;
+                    i = num;
+                    ++cnt;
+                }
+                ans = Math.max(ans, cnt);
+            }
+            return ans;
+        }
+    }
+
 }
