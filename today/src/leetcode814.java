62f34750244cee629f756b720620cbd3ca06209f
xiaozhao <2502712438@qq.com>
2022-07-21 11:22:35 +0800

    每日7.21

---
+++ today/src/leetcode814.java
@@ -0,0 +1,63 @@
+/*
+ * 814. 二叉树剪枝
+ * */
+public class leetcode814 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (dfs(root) == 0) {
+            return null;
+        }
+        return root;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        if (left == 0) {
+            node.left = null;
+        }
+        if (right == 0) {
+            node.right = null;
+        }
+        return left + right + node.val;
+    }
+
+    //官方解答
+    class Solution {
+        public TreeNode pruneTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            root.left = pruneTree(root.left);
+            root.right = pruneTree(root.right);
+            if (root.left == null && root.right == null && root.val == 0) {
+                return null;
+            }
+            return root;
+        }
+    }
+
+
+}
