dfb1afaf18f58ec43e4e883c7d3aefc829a1fcce
xiaozhao <2502712438@qq.com>
2022-08-19 18:18:42 +0800

    每日8.19

---
+++ today/src/leetcode1450.java
@@ -0,0 +1,17 @@
+/*
+ * 1450. 在既定时间做作业的学生人数
+ * */
+public class leetcode1450 {
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
+}
