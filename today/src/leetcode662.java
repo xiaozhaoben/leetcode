09834b6d7f4df0fe77bd45c3b9d728d31ff268ac
xiaozhao <2502712438@qq.com>
2022-08-27 13:49:24 +0800

    每日8.27

---
+++ today/src/leetcode662.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * 662. 二叉树最大宽度
+ * */
+public class leetcode662 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Map<Integer, Integer> levelMin = new HashMap<Integer, Integer>();
+
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 1, 1);
+    }
+
+    public int dfs(TreeNode node, int depth, int index) {
+        if (node == null) {
+            return 0;
+        }
+        levelMin.putIfAbsent(depth, index); // 每一层最先访问到的节点会是最左边的节点，即每一层编号的最小值
+        return Math.max(index - levelMin.get(depth) + 1, Math.max(dfs(node.left, depth + 1, index * 2), dfs(node.right, depth + 1, index * 2 + 1)));
+    }
+
+
+}
