4dc0ea676aa042bac528985d694175fdd6aa584e
xiaozhao <2502712438@qq.com>
2022-09-03 15:01:01 +0800

    选择题

---
+++ practice/src/choiceQuestion/Print.java
@@ -0,0 +1,16 @@
+package choiceQuestion;
+
+public class Print {
+    static boolean out(char c) {
+        System.out.print(c);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (out('A'); out('B') && i < 2; out('C')) {
+            i++;
+            out('D');
+        }
+    }
+}
