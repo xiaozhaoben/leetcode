3f31fed4c7431e3a58947c4bfd3b51366fb37dd0
xiaozhao <2502712438@qq.com>
2022-09-06 17:11:40 +0800

    单例模式

---
+++ practice/src/designPattern/SinglePattern/Singleton.java
@@ -0,0 +1,24 @@
+package designPattern.SinglePattern;
+
+public class Singleton {
+
+    private volatile static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    return new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("双重校验");
+    }
+}
