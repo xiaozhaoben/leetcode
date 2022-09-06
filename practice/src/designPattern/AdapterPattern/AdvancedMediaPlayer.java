243c212bca59152571b52b42e2e72c04078c1cea
xiaozhao <2502712438@qq.com>
2022-09-06 18:54:08 +0800

    适配器模式

---
+++ practice/src/designPattern/AdapterPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package designPattern.AdapterPattern;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
---
+++ practice/src/designPattern/AdapterPattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package designPattern.AdapterPattern;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
