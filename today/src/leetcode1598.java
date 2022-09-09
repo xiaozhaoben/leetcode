633190c8701ea62f3ffcd427aacf6de9398f1379
xiaozhao <2502712438@qq.com>
2022-09-09 09:59:46 +0800

    每日

---
+++ today/src/leetcode1598.java
@@ -0,0 +1,23 @@
+/*
+ * 1598. 文件夹操作日志搜集器
+ * */
+public class leetcode1598 {
+
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs) {
+            if ("./".equals(log)) {
+                continue;
+            } else if ("../".equals(log)) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else {
+                depth++;
+            }
+        }
+        return depth;
+    }
+
+
+}
